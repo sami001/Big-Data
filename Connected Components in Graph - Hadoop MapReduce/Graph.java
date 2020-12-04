4962a93936a43cffdfd857265b430c86d97c464a
Mohammed Samiul Saeef <saeef.sami@gmail.com>
2020-12-04 13:41:10 -0600

    Update Graph.java

--- Connected Components in Graph - Hadoop MapReduce/Graph.java
+++ Connected Components in Graph - Hadoop MapReduce/Graph.java
@@ -1,3 +1,42 @@
+/*********************************** PSEUDOCODE ***************************************
+class Vertex extends Writable {
+  short tag;                 // 0 for a graph vertex, 1 for a group number
+  long group;                // the group where this vertex belongs to
+  long VID;                  // the vertex ID
+  Vector adjacent;     // the vertex neighbors
+  ...
+}
+Vertex must have two constructors: Vertex(tag,group,VID,adjacent) and Vertex(tag,group).
+First Map-Reduce job:
+
+map ( key, line ) =
+  parse the line to get the vertex VID and the adjacent vector
+  emit( VID, new Vertex(0,VID,VID,adjacent) )
+Second Map-Reduce job:
+map ( key, vertex ) =
+  emit( vertex.VID, vertex )   // pass the graph topology
+  for n in vertex.adjacent:
+     emit( n, new Vertex(1,vertex.group) )  // send the group # to the adjacent vertices
+
+reduce ( vid, values ) =
+  m = Long.MAX_VALUE;
+  for v in values:
+     if v.tag == 0
+        then adj = v.adjacent.clone()     // found the vertex with vid
+     m = min(m,v.group)
+  emit( m, new Vertex(0,m,vid,adj) )      // new group #
+Final Map-Reduce job:
+map ( group, value ) =
+   emit(group,1)
+
+reduce ( group, values ) =
+   m = 0
+   for v in values
+       m = m+v
+   emit(group,m)
+The second map-reduce job must be repeated multiple times.
+*/
+
 package edu.uta.cse6331;
 import java.util.*;
 import java.io.*;
